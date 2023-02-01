d5573d758ac3d04bd8659da9c4503821d3094573
Theo Levequet <theo.leveque@gmail.com>
2023-01-31 16:00:38 -0800

    chore: use .env for API keys

--- src/utils/airtable.ts
+++ src/utils/airtable.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const airtable = axios.create({
 	baseURL: 'https://api.airtable.com/v0/appYL0R3gD38Dcjzr/AI%20Tools',
 	headers: {
-		Authorization:
-			'Bearer patOOweXf5U89TKrX.00cce371a149cf26f71940681cc45075ef678bf44569484e863ba4808c6aca86'
+		Authorization: `Bearer ${process.env.AIRTABLE_ACCESS_TOKEN}`
 	}
 });
--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -4,6 +4,6 @@ export const openai = axios.create({
 	baseURL: 'https://api.openai.com/v1/completions',
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: 'Bearer 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'
+		Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
 	}
 });

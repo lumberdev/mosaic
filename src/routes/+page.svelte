6c7a4b5c33fb2ad17cfa303fcc56161d1cdb6986
Theo Levequet <theo.leveque@gmail.com>
2023-02-03 14:59:39 -0800

    feat: add tool list on home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,18 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	let { tools } = data;
+	$: ({ tools } = data);
 </script>
 
 <div class="grid min-h-screen place-items-center">
 	<h1 class="text-center text-3xl">This is Mosaic!</h1>
-
-	<pre>{JSON.stringify(data.tools, null, 2)}</pre>
+	<ul>
+		{#each tools as { title, url }}
+			<li class="flex items-center gap-1">
+				<h2 class="text-lg font-semibold">{title}</h2>
+				<a href={url} class="text-blue-700 underline">Live site</a>
+			</li>
+		{/each}
+	</ul>
 </div>

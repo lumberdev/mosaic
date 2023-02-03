c75898bd3e9dce4174d1048422f77a2cac1ae8fc
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2023-02-03 17:23:57 -0600

    chore: Remove unused code

--- src/utils/airtable.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { env } from '$env/dynamic/private';
-
-export const airtable = axios.create({
-	baseURL: 'https://api.airtable.com/v0',
-	headers: {
-		Authorization: `Bearer ${env.AIRTABLE_ACCESS_TOKEN}`
-	}
-});

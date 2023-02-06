eb738686e5d743744888351d0994df99b52e5b4e
Fernando Saavedra <fernando@fsvdr.me>
2023-02-06 12:46:02 -0600

    Feature/homepage (#2)
    * feat: supabase wiring + type gen
    
    * chore: Remove unused code
    
    * docs: Add env variables template file
    
    * feat: Display tools in homepage grid
    
    * fix: Restore code prior to rebase
    
    ---------
    
    Co-authored-by: Theo Levequet <theo.leveque@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+  import "@fontsource/unbounded"
 </script>
 
+<header class="flex flex-col justify-center items-center py-16 lg:py-24">
+  <h1 class="font-display uppercase u-text-gradient u-background-gradient text-6xl mb-8 lg:text-9xl">Mos<span class="inline-block -mt-4 transform -translate-y-4">a</span><span class="inline-block -mb-4 transform translate-y-4">i</span>c</h1>
+  <p class="tracking-normal lg:text-4xl">Navigating the world of AI</p>
+</header>
 <slot />

766020f708915bf14a66d090a82996dd36e6c5e2
Theo Levequet <theo.leveque@gmail.com>
2023-02-07 14:06:13 -0800

    feat: add basic nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,17 @@
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 
+	const navLinks = [
+		{
+			name: 'Home',
+			href: '/'
+		},
+		{
+			name: 'Admin',
+			href: '/admin'
+		}
+	];
+
 	onMount(() => {
 		const {
 			data: { subscription }
@@ -17,7 +28,16 @@
 	});
 </script>
 
-<header class="flex flex-col items-center justify-center py-16 lg:py-24">
+<nav>
+	<ul class="flex gap-8 py-4 px-4 lg:px-8 lg:py-8">
+		{#each navLinks as { name, href }}
+			<li>
+				<a {href}>{name}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
+<header class="flex flex-col items-center justify-center py-8 lg:py-16">
 	<h1 class="mb-8 text-6xl font-semibold uppercase lg:text-9xl">Mosaic</h1>
 	<p class="tracking-normal lg:text-4xl">Navigating the world of AI</p>
 </header>

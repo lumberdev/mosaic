38456dd8a2e188ddf8da95484d79ff11d66b77ec
Theo Levequet <theo.leveque@gmail.com>
2023-02-07 08:51:50 -0800

    chore: cleanup

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,6 @@ import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await getServerSession(event);
-	console.log('🚀 ~ file: +layout.server.ts:6 ~ constload:LayoutServerLoad= ~ session', session);
 	return {
 		session
 	};
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,6 +6,5 @@ import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutLoad = async (event) => {
 	const { session } = await getSupabase(event);
-	console.log('🚀 ~ file: +layout.ts:9 ~ constload:LayoutLoad= ~ session', session);
 	return { session };
 };

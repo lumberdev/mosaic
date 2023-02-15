89ab056f894b05f583b75195dcab0f179e5d6e2c
Theo Levequet <theo.leveque@gmail.com>
2023-02-14 16:34:28 -0800

    chore: add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				display: ['Unbounded', ...defaultTheme.fontFamily.sans],
+				display: ['Silkscreen', ...defaultTheme.fontFamily.sans],
+				sans: ['"Spline Sans"', ...defaultTheme.fontFamily.sans],
 			},
 			borderWidth: {
 				3: '3px',
@@ -15,7 +16,8 @@ module.exports = {
 				DEFAULT: '10px',
 			},
 			boxShadow: {
-				DEFAULT: '5px 5px 0px #000000',
+				md: '5px 5px 0px #000000',
+				sm: '3px 3px 0px #000000',
 			},
 			colors: {
 				beige: '#E2E8E4',

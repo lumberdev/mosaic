acd6a098cfd6ebfe0247df840c10377ced1923a4
Theo Levequet <theo.leveque@gmail.com>
2023-03-08 13:25:03 -0800

    feat: remove one password field

--- src/components/Admin/SetPassword.svelte
+++ src/components/Admin/SetPassword.svelte
@@ -3,30 +3,20 @@
 	import FormWrapper from '../Forms/FormWrapper.svelte';
 	import Input from '../Forms/Input.svelte';
 
-	const passwordInputAttributes = Array.from({ length: 2 }, (v, i) => ({
+	const passwordInputAttributes = {
 		type: 'password',
 		placeholder: 'Enter your new password here',
-		id: `password${i + 1}`,
-		name: `password${i + 1}`,
+		id: 'password',
+		name: 'password',
 		value: '',
-	}));
-	const buttonLabel = 'Set Password';
-	const arePasswordsTheSame = () => {
-		const [password1, password2] = passwordInputAttributes.map(
-			(inputAttributes) => inputAttributes.value
-		);
-		return password1 === password2;
 	};
+
+	const buttonLabel = 'Set Password';
 </script>
 
 <FormWrapper title="Set a password">
 	<form method="post" action="?/setPassword" class="grid w-full items-center gap-4  ">
-		{#each passwordInputAttributes as inputAttributes}
-			<Input {...inputAttributes} bind:value={inputAttributes.value} />
-			{#if !arePasswordsTheSame()}
-				<p class="text-red-500">Passwords do not match</p>
-			{/if}
-		{/each}
+		<Input {...passwordInputAttributes} bind:value={passwordInputAttributes.value} />
 		<Button {buttonLabel} />
 	</form>
 </FormWrapper>

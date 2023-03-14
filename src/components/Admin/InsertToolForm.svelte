dd2b9dba653cca758b6ea2b24ce160e1a6f754a5
Théo Lévêque <70675996+daawascript@users.noreply.github.com>
2023-03-14 16:32:57 -0700

    feat: add playwright-aws-lambda (#13)
    * feat: add playwright-aws-lambda
    
    * feat: add dev + prod modes
    
    * style: fix overflow
    
    * chore: update .gitignore to catch yarn error file

--- src/components/Admin/InsertToolForm.svelte
+++ src/components/Admin/InsertToolForm.svelte
@@ -118,128 +118,128 @@
 	};
 </script>
 
-<form
-	use:enhance
-	method="POST"
-	action="?/insert"
-	enctype="multipart/form-data"
-	class="mx-auto flex w-max flex-col items-center justify-center pt-8">
-	<h1 class="mb-4 font-display text-5xl">Found a new cool AI tool?</h1>
-	<p class="mb-8">Let's add it to Mosaic!</p>
+<div class="mx-auto max-w-3xl">
+	<form
+		use:enhance
+		method="POST"
+		action="?/insert"
+		enctype="multipart/form-data"
+		class="mx-auto flex w-max  flex-col items-center justify-center pt-8">
+		<h1 class="mb-4 font-display text-5xl">Found a new cool AI tool?</h1>
+		<p class="mb-8">Let's add it to Mosaic!</p>
 
-	<div class="mb-8 w-max">
-		<p class="mb-4 text-center font-display">How do you want to summarize this website?</p>
+		<div class="mb-8 w-max">
+			<p class="mb-4 text-center font-display">How do you want to summarize this website?</p>
 
-		<div class="grid w-full grid-cols-3 gap-2">
-			<label class="rounded border-3 border-black bg-white px-5 py-4">
-				<input
-					type="radio"
-					bind:group={generationMethod}
-					name="method"
-					value="cached-content"
-					checked />
-				<span>Cached Content</span>
+			<div class="grid w-full grid-cols-3 gap-2">
+				<label class="rounded border-3 border-black bg-white px-5 py-4">
+					<input
+						type="radio"
+						bind:group={generationMethod}
+						name="method"
+						value="cached-content"
+						checked />
+					<span>Cached Content</span>
+				</label>
+
+				<label class="rounded border-3 border-black bg-white px-5 py-4">
+					<input type="radio" bind:group={generationMethod} name="method" value="content" />
+					<span>Website's Content</span>
+				</label>
+
+				<label class="rounded border-3 border-black bg-white px-5 py-4">
+					<input type="radio" bind:group={generationMethod} name="method" value="meta" />
+					<span>Website Meta Tag</span>
+				</label>
+
+				<label class="rounded border-3 border-black bg-white px-5 py-4">
+					<input type="radio" bind:group={generationMethod} name="method" value="url" />
+					<span>URL</span>
+				</label>
+			</div>
+		</div>
+
+		<label class="c-field w-full">
+			<span class="c-field-label">URL</span>
+			<input
+				class="c-field-input"
+				type="url"
+				name="url"
+				bind:value={url}
+				placeholder="https://example.com"
+				required />
+		</label>
+
+		<div class="mb-12 w-full">
+			<button
+				type="button"
+				class="c-btn-submit w-full disabled:cursor-not-allowed disabled:opacity-20"
+				on:click={generateContentClientSide}
+				disabled={isLoading}>
+				Generate content
+			</button>
+		</div>
+
+		<label class="c-field w-full">
+			<span class="c-field-label">Name</span>
+			<input
+				class="c-field-input"
+				type="name"
+				name="name"
+				bind:value={name}
+				placeholder="example"
+				required />
+		</label>
+
+		<label class="c-field w-full">
+			<span class="c-field-label">Description</span>
+			<textarea
+				class="c-field-input"
+				name="description"
+				placeholder="Brief description..."
+				bind:value={description}
+				required />
+		</label>
+
+		<label class="c-field w-full">
+			<span class="c-field-label">Tags</span>
+			<input class="c-field-input" type="name" name="tags" bind:value={tags} />
+		</label>
+
+		<div class="flex gap-4">
+			<label class="c-field">
+				<span class="c-field-label">Upload Image</span>
+				<input type="radio" name="uploadImage" bind:group={uploadImage} value={true} />
 			</label>
-
-			<label class="rounded border-3 border-black bg-white px-5 py-4">
-				<input type="radio" bind:group={generationMethod} name="method" value="content" />
-				<span>Website's Content</span>
-			</label>
-
-			<label class="rounded border-3 border-black bg-white px-5 py-4">
-				<input type="radio" bind:group={generationMethod} name="method" value="meta" />
-				<span>Website Meta Tag</span>
-			</label>
-
-			<label class="rounded border-3 border-black bg-white px-5 py-4">
-				<input type="radio" bind:group={generationMethod} name="method" value="url" />
-				<span>URL</span>
+			<label class="c-field">
+				<span class="c-field-label">Generate Image</span>
+				<input type="radio" name="uploadImage" bind:group={uploadImage} value={false} />
 			</label>
 		</div>
-	</div>
 
-	<label class="c-field w-full">
-		<span class="c-field-label">URL</span>
-		<input
-			class="c-field-input"
-			type="url"
-			name="url"
-			bind:value={url}
-			placeholder="https://example.com"
-			required />
-	</label>
-
-	<div class="mb-12 w-full">
-		<button
-			type="button"
-			class="c-btn-submit w-full disabled:cursor-not-allowed disabled:opacity-20"
-			on:click={generateContentClientSide}
-			disabled={isLoading}>
-			Generate content
-		</button>
-	</div>
-
-	<label class="c-field w-full">
-		<span class="c-field-label">Name</span>
-		<input
-			class="c-field-input"
-			type="name"
-			name="name"
-			bind:value={name}
-			placeholder="example"
-			required />
-	</label>
-
-	<label class="c-field w-full">
-		<span class="c-field-label">Description</span>
-		<textarea
-			class="c-field-input"
-			name="description"
-			placeholder="Brief description..."
-			bind:value={description}
-			required />
-	</label>
-
-	<label class="c-field w-full">
-		<span class="c-field-label">Tags</span>
-		<input class="c-field-input" type="name" name="tags" bind:value={tags} />
-	</label>
-
-	<div class="flex gap-4">
-		<label class="c-field">
-			<span class="c-field-label">Upload Image</span>
-			<input type="radio" name="uploadImage" bind:group={uploadImage} value={true} />
-		</label>
-		<label class="c-field">
-			<span class="c-field-label">Generate Image</span>
-			<input type="radio" name="uploadImage" bind:group={uploadImage} value={false} />
-		</label>
-	</div>
-
-	{#if uploadImage}
-		<label class="c-field w-full">
-			<span class="c-field-label">Featured Image</span>
-			<input type="file" class="c-field-input" name="image" bind:value={file} required />
-		</label>
-	{:else}
-		<button type="button" class="c-btn-submit mb-4 w-full" on:click={generateImage}
-			>Generate Image</button>
-		{#if imageUrl}
-			<img src={imageUrl} alt={`${url}\'s screenshot`} class="w-full" />
+		{#if uploadImage}
 			<label class="c-field w-full">
-				<span class="c-field-label">Featured Image Path</span>
-				<input
-					type="text"
-					class="c-field-input"
-					name="imagePath"
-					bind:value={imagePath}
-					required
-					readonly />
+				<span class="c-field-label">Featured Image</span>
+				<input type="file" class="c-field-input" name="image" bind:value={file} required />
 			</label>
+		{:else}
+			<button type="button" class="c-btn-submit mb-4 w-full" on:click={generateImage}
+				>Generate Image</button>
+			{#if imageUrl}
+				<img src={imageUrl} alt={`${url}\'s screenshot`} class="w-full max-w-3xl" />
+				<label class="c-field w-full">
+					<span class="c-field-label">Featured Image Path</span>
+					<input
+						type="text"
+						class="c-field-input"
+						name="imagePath"
+						bind:value={imagePath}
+						required
+						readonly />
+				</label>
+			{/if}
 		{/if}
-	{/if}
 
-	<button type="submit" class="c-btn-submit w-full">Submit</button>
-</form>
-
-<!-- https://qgynrzoywcnqzqkulybg.supabase.co/storage/v1/object/public/tools-images/More_thoughtfully_crafted_features_for_Gmail.png -->
+		<button type="submit" class="c-btn-submit w-full">Submit</button>
+	</form>
+</div>

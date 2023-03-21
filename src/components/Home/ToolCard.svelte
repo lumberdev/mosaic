eab7f21462dd325ced8231c13e1c4ed8dc2ab17f
Theo Levequet <theo.leveque@gmail.com>
2023-03-21 15:22:56 -0700

    style: fix border radii + uniformize grid height

--- src/components/Home/ToolCard.svelte
+++ src/components/Home/ToolCard.svelte
@@ -4,23 +4,26 @@
 	import Arrow from '../SVG/Arrow.svelte';
 	export let tool: Tool;
 
-	$: ({ name, url, featured_image, slug, description } = tool);
+	const { name, featured_image, slug, description } = tool;
 	$: image_url = PUBLIC_SUPABASE_URL + '/storage/v1/object/public/tools-images/' + featured_image;
+
+	const shortDescription = description?.split(' ').slice(0, 20).join(' ') + '...';
 </script>
 
 <a class="group" href={`/tools/${slug}`}>
 	<article
-		class="aspect-square rounded border-3 border-black transition-shadow group-hover:shadow-md">
+		class="flex h-full flex-col rounded border-3 border-black transition-shadow group-hover:shadow-md">
 		<div
 			class="flex justify-between border-b-3 border-black px-5 py-3 group-hover:bg-black group-hover:text-white">
-			<span class="font-display">Free</span>
+			<span class="font-display" />
 			<Arrow className="[&_path]:group-hover:fill-white" />
 		</div>
 		<img class="aspect-video w-full" src={image_url} alt={name} />
-		<div class="rounded border-t-3 border-black p-5 transition-colors group-hover:bg-beige">
-			<h2 class="mb-[10px] text-2xl font-bold">{name}</h2>
-			<p class="mb-5">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, repellendus?
+		<div
+			class="flex-auto rounded-b border-t-3 border-black p-5 transition-colors group-hover:bg-beige">
+			<h2 class="mb-3 text-2xl font-bold">{name}</h2>
+			<p class="mb-5 ">
+				{shortDescription}
 			</p>
 		</div>
 	</article>

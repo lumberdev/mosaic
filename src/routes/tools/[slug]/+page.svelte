3b70a466a3f75f0d3e7db8db9ab86c1db908d15d
Theo Levequet <theo.leveque@gmail.com>
2023-03-23 13:27:55 -0700

    style: fix image rounded corners

--- src/routes/tools/[slug]/+page.svelte
+++ src/routes/tools/[slug]/+page.svelte
@@ -17,8 +17,8 @@
 
 <section class="relative grid gap-5 md:grid-cols-12">
 	<div
-		class="aspect-[660/433] rounded border-3  border-black object-cover shadow-md md:col-start-2 md:col-end-8  md:mb-12">
-		<img class="h-full overflow-hidden rounded" src={image_url} alt={name} />
+		class="aspect-[660/433] overflow-hidden rounded border-3 border-black object-cover shadow-md md:col-start-2 md:col-end-8  md:mb-12">
+		<img class="h-full overflow-hidden " src={image_url} alt={name} />
 	</div>
 	<div
 		class="relative mt-6 grid gap-8 rounded px-6 pt-6 pb-16 md:col-start-8 md:col-end-12 md:mt-12 md:pt-9 md:pb-24 md:pl-5 md:pr-12">

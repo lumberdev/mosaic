b1e21f0358fc10bb50ab1a58d4bd9ee99b978654
Théo Lévêque <70675996+daawascript@users.noreply.github.com>
2023-03-29 08:41:30 -0700

    style/tool page (#16)
    * style: fix image rounded corners
    
    * feat: remove pricing from card

--- src/routes/tools/[slug]/+page.svelte
+++ src/routes/tools/[slug]/+page.svelte
@@ -10,15 +10,15 @@
 
 	const { tool } = data;
 	const { name, description, url, featured_image } = tool;
-	const pricing = 'unknown';
+	// const pricing = 'unknown';
 	const image_url =
 		PUBLIC_SUPABASE_URL + '/storage/v1/object/public/tools-images/' + featured_image;
 </script>
 
 <section class="relative grid gap-5 md:grid-cols-12">
 	<div
-		class="aspect-[660/433] rounded border-3  border-black object-cover shadow-md md:col-start-2 md:col-end-8  md:mb-12">
-		<img class="h-full overflow-hidden rounded" src={image_url} alt={name} />
+		class="aspect-[660/433] overflow-hidden rounded border-3 border-black object-cover shadow-md md:col-start-2 md:col-end-8  md:mb-12">
+		<img class="h-full overflow-hidden " src={image_url} alt={name} />
 	</div>
 	<div
 		class="relative mt-6 grid gap-8 rounded px-6 pt-6 pb-16 md:col-start-8 md:col-end-12 md:mt-12 md:pt-9 md:pb-24 md:pl-5 md:pr-12">
@@ -27,7 +27,7 @@
 			<a class="font-display" target="_blank" rel="noreferrer noopener" href={url}>{url}</a>
 		</div>
 		<p>{description}</p>
-		<p class="font-display">Pricing: <span>{pricing}</span></p>
+		<!-- <p class="font-display">Pricing: <span>{pricing}</span></p> -->
 		<div
 			class="absolute top-0 right-0 -z-10 grid h-full w-full rounded border-3 border-black bg-white md:w-[calc(100%+52%)]">
 			<div class="h-12 w-full self-end bg-black" />

8aeb6ea87ba6e66ed158a38483591011152c872b
Theo Levequet <theo.leveque@gmail.com>
2023-04-07 10:54:41 -0700

    fix: image path

--- src/routes/tools/[slug]/+page.svelte
+++ src/routes/tools/[slug]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 	import SubmitAi from '../../../components/Forms/SubmitAI.svelte';
 
 	import type { ActionData } from '../../$types'; //Submit tool Action is set at the root
@@ -9,16 +8,17 @@
 	export let data: PageData;
 
 	const { tool } = data;
-	const { name, description, url, featured_image } = tool;
+	const { name, description, url, featuredImage } = tool;
 	// const pricing = 'unknown';
-	const image_url =
-		PUBLIC_SUPABASE_URL + '/storage/v1/object/public/tools-images/' + featured_image;
 </script>
 
 <section class="relative grid gap-5 md:grid-cols-12">
 	<div
 		class="aspect-[660/433] overflow-hidden rounded border-3 border-black object-cover shadow-md md:col-start-2 md:col-end-8  md:mb-12">
-		<img class="h-full overflow-hidden " src={image_url} alt={name} />
+		<img
+			class="h-full overflow-hidden "
+			src={featuredImage.url}
+			alt={featuredImage.description ?? name} />
 	</div>
 	<div
 		class="relative mt-6 grid gap-8 rounded px-6 pt-6 pb-16 md:col-start-8 md:col-end-12 md:mt-12 md:pt-9 md:pb-24 md:pl-5 md:pr-12">

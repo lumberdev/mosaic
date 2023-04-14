890d57514ca331dcd2dcd3b3a8e693260b7a84ed
Theo Levequet <theo.leveque@gmail.com>
2023-04-14 11:21:26 -0700

    feat: return only values from contentful entries

--- src/routes/api/upload-to-contentful/+server.ts
+++ src/routes/api/upload-to-contentful/+server.ts
@@ -6,18 +6,22 @@ import type { GenerateContentResponse } from '$lib/types';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const allContentPromisesResolved = await request.json();
-
-	let allContentfulEntryPromisesResolved: PromiseSettledResult<Entry>[];
+	let allContentfulEntries: Entry[];
 	try {
-		allContentfulEntryPromisesResolved = await Promise.allSettled(
-			allContentPromisesResolved.map((contentResponse: GenerateContentResponse) => {
-				const contentfulObject = serializeEntry(contentResponse.data);
-				return createEntry('tool', contentfulObject);
-			})
-		);
+		const allContentfulEntryPromisesResolved: PromiseSettledResult<Entry>[] =
+			await Promise.allSettled(
+				allContentPromisesResolved.map((contentResponse: GenerateContentResponse) => {
+					const contentfulObject = serializeEntry(contentResponse.data);
+					return createEntry('tool', contentfulObject);
+				})
+			);
+		const allContentfulEntriesFulfilled = allContentfulEntryPromisesResolved.filter(
+			(promise) => promise.status === 'fulfilled'
+		) as PromiseFulfilledResult<Entry>[];
+		allContentfulEntries = allContentfulEntriesFulfilled.map((promise) => promise.value);
 	} catch (error) {
 		console.error(error);
 		throw svelteError(500, 'Server error while uploading content. Try again later.');
 	}
-	return json(allContentfulEntryPromisesResolved);
+	return json(allContentfulEntries);
 };

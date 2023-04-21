8b7722824440a675057fc201eaa2be0d24629ed4
Theo Levequet <theo.leveque@gmail.com>
2023-04-21 09:53:11 -0700

    feat: move to chat GPT

--- src/lib/utils/public-openai.ts
+++ src/lib/utils/public-openai.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { env } from '$env/dynamic/public';
 
 export const DANGEROUSLY_PUBLIC_openai = axios.create({
-	baseURL: 'https://api.openai.com/v1/completions',
+	baseURL: 'https://api.openai.com/v1/chat/completions',
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: `Bearer ${env.PUBLIC_OPENAI_API_KEY}`
-	}
+		Authorization: `Bearer ${env.PUBLIC_OPENAI_API_KEY}`,
+	},
 });
